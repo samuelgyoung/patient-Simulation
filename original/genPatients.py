44ed080668f77fc1aae5128fd7d4d860f87bc4ef
Garrett Young <sgyoung@us.ibm.com>
2018-10-30 13:35:45 -0500

    Adding original from https://github.com/diarmaidocualain/ecg_simulation

---
+++ original/genPatients.py
@@ -0,0 +1,12 @@
+
+from patient import Patient
+
+import json
+
+p1 = Patient("Gart", 36, 1004)
+#print(p1.name)
+##print(p1.ecgStat)
+print(p1.ecgStat)
+#json_string = json.dumps(p1.ecgStat.tolist())
+
+#json_string
---
+++ original/patient.py
@@ -0,0 +1,58 @@
+import pylab
+import scipy.signal as signal
+import numpy
+import random
+
+import json
+
+class Patient:
+   def __init__(self, name, age, room):
+        self.name = name
+        self.age = age
+        self.room = room
+        self.beatsPerMinute = random.randint(60,130)
+        self.ecgStat = self.ecg(self.beatsPerMinute)
+
+   def ecg(self, beatsPerMinute = 60):
+      # The "Daubechies" wavelet is a rough approximation to a real,
+      # single, heart beat ("pqrst") signal
+      pqrst = signal.wavelets.daub(10)
+
+      # Add the gap after the pqrst when the heart is resting.
+      samples_rest = 10
+      zero_array = numpy.zeros(samples_rest, dtype=float)
+      pqrst_full = numpy.concatenate([pqrst,zero_array])
+
+      # Simulated Beats per minute rate
+      # For a health, athletic, person, 60 is resting, 180 is intensive exercising
+      bpm = beatsPerMinute
+      bps = bpm / 60
+
+      # Simumated period of time in seconds that the ecg is captured in
+      capture_length = 10
+
+      # Caculate the number of beats in capture time period
+      # Round the number to simplify things
+      num_heart_beats = int(capture_length * bps)
+
+      # Concatonate together the number of heart beats needed
+      ecg_template = numpy.tile(pqrst_full , num_heart_beats)
+
+      # Add random (gaussian distributed) noise
+      noise = numpy.random.normal(0, 0.01, len(ecg_template))
+      ecg_template_noisy = noise + ecg_template
+
+      # Simulate an ADC by sampling the noisy ecg template to produce the values
+      # Might be worth checking nyquist here
+      # e.g. sampling rate >= (2 * template sampling rate)
+      sampling_rate = 50.0
+      num_samples = sampling_rate * capture_length
+      ecg_sampled = signal.resample(ecg_template_noisy, int(num_samples))
+
+      # Scale the normalised amplitude of the sampled ecg to whatever the ADC
+      # bit resolution is
+      # note: check if this is correct: not sure if there should be negative bit values.
+      adc_bit_resolution = 1024
+      ecg =  adc_bit_resolution * ecg_sampled
+
+      return jsonify(str(ecg.tolist()))
